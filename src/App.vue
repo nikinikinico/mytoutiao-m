d36f81fc903130bbda8472d3982a8bc9b697d418
nikinikinico <1429020455@qq.com>
2022-11-27 17:33:45 +0800

    引入vant组件

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,27 @@
 <template>
   <div id="app">
     <!-- 路由的出口 -->
-    <router-view/>
+    <router-view />
     黑马头条
+    <div>
+      <i class="toutiao toutiao-shanchu"></i>
+      <van-button type="primary">主要按钮</van-button>
+      <van-button type="info">信息按钮</van-button>
+      <van-button type="default">默认按钮</van-button>
+      <van-button type="warning">警告按钮</van-button>
+      <van-button type="danger">危险按钮</van-button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'APP'
+  name: 'APP',
+  data() {
+    return {}
+  }
 }
 </script>
 
-<style lang="less"></style>
+<style lang="less">
+</style>
